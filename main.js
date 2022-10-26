93caee130cb8c43aad94a3cde9d2cfa42bd3555c
Kisakoroleva <tran54m3r@gmail.com>
2022-10-26 21:54:48 +0700

    last upd

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+var myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  var mySrc = myImage.getAttribute("src");
+  if (mySrc === "2.jpg") {
+    myImage.setAttribute("src", "1.jpg");
+  } else {
+    myImage.setAttribute("src", "2.jpg");
+  }
+};
+
+var myButton = document.querySelector("button");
+var myHeading = document.querySelector("h1");
+
+function setUserName() {
+  var myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = "Mozilla is cool, " + myName;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem("name");
+  myHeading.textContent = "Mozilla is cool, " + storedName;
+}
+
+myButton.onclick = function () {
+  setUserName();
+};
